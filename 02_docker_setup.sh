48cc94f783941e0e23f298b2e4d9917fc512444c
Tim <1312348+turgs@users.noreply.github.com>
2020-08-13 12:51:23 +1000

    Update 02_docker_setup.sh

--- 02_docker_setup.sh
+++ 02_docker_setup.sh
@@ -47,12 +47,4 @@ else
 fi
 
 
-# contigure docker to work with UFW
-# https://github.com/chaifeng/ufw-docker#solving-ufw-and-docker-issues
-####################################################################
-sudo wget -O /usr/local/bin/ufw-docker \
-  https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
-chmod +x /usr/local/bin/ufw-docker
-ufw-docker install
-sudo systemctl restart ufw
 
